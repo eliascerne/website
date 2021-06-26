cce3d74c142152815f7443c42e8e5fa817dcacc6
erias04 <lichtbargeld@gmail.com>
2021-06-26 12:28:15 +0200

    feat: main.js: toggle Skills open and close This is the full code for toggling the Skills open and close.
    First, we get the Elements by getElementsByClassName, that was wrong , for the last time. The last commit I said getElementByClassName, but thats weong, right would be getLementsByClassName.
    
    Then we have a function toggleSkills. That function works with 0 and 1.
    
    The last thing has an EventListener, if we click on a Skill, it toggles 'toggleSkills' and it opens. All the others will close.

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,5 +28,20 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SKILLS =====*/
-const skillsContent = documet.getElementByClassName('skills_content'),
+const skillsContent = document.getElementsByClassName('skills_content'),
       skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
