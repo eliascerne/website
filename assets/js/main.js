354e603ffbe483eac60520e26d3fddeb06b6e599
erias04 <lichtbargeld@gmail.com>
2021-06-27 12:57:47 +0200

    feat: main.js, index.html: hide/show when clicking on qualification_section For JS: We first get the classes from HTML, then add an EventListener, which looks if we click on the section. If we click on the sections, it removes or add qualification_active.
    For HTML: added data-target to the qualification_buttons for the sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,19 @@ skillsHeader.forEach((el) =>{
 /*===== QUALIFICATION TABS =====*/
 const tabs = document.querySelectorAll('[data-target]'),
       tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
