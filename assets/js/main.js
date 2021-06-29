519d30f878a6e24a2bcc3680284177881d27bd9e
erias04 <lichtbargeld@gmail.com>
2021-06-29 15:15:29 +0200

    delete: main.js: portfolio_swiperjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,15 +90,3 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*===== PORTFOLIO SWIPER =====*/
-var swiperPortfolio = new Swiper('.mySwiper', {
-    cssMode: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-})
