80d954c55bcb4867ed93252260e83eac9f875132
erias04 <lichtbargeld@gmail.com>
2021-07-02 22:15:08 +0200

    feat: main.js: config DARK_LIGHT THEME First were added two lines to store information on the localStorage, the localStorage is used that the theme and icon is stored, that if you close the tab or even the whole Program, it will store the selected-theme and selected-icon. SessionStorage is also avaiable and used to only store data till the tab is closed (we do not want to do that here). After that we creating new constant varables and give them a reference

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,3 +122,11 @@ window.addEventListener('scroll', scrollUp)
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
