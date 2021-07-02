65e2e37bfd6878b33b0927a092a517f784bac809
erias04 <lichtbargeld@gmail.com>
2021-07-02 20:58:33 +0200

    feat: styles.css, main.js: show scrollup when moving down This code lets the scrollup first come, when scrolling a little bit down FOR CSS: chnage the bottom to: -20%, that it is not showing normally, and show the scrollup, made with .show-scroll, there the bottom is chnaged to 5rem FOR JS: create a function which gets the element from html 'scroll-up' and when the scroller is higher than 560 vh (viewport height), then the bottom of scrollup chnages to 5rem (css above)

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,3 +109,11 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+/* SHOW SCROLL TOP */
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
