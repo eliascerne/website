36d36fa927cc7644c26258b7d3411ad6553d1334
erias04 <lichtbargeld@gmail.com>
2021-07-03 21:29:38 +0200

    style: main.js: chnage comment Chnage the comment from 200vh (viewport height) to 80vh

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,7 +121,7 @@ window.addEventListener('scroll', scrollUp)
 /* CHANGE BACKGROUND HEADER */
 function scrollHeader(){
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
