0aed9cea2de291da6656a4ff05a054062d0ffdea
erias04 <lichtbargeld@gmail.com>
2021-07-03 21:21:45 +0200

    feat: main.js: shadow when scrolling down for nav Js for the scrolling down shadow effect for the navigation bar. This script is a function which calls scrollHeader and at first he is getting from an id in html the nav. The there is a if statement. If the scrollY, so if the user scrolled down for 80 or more, he will add a new class to the header called scroll-header. Scroll-header will be configured in css later on. The else function is for 80 or less, that it will remove the class and with that the shadow

--- assets/js/main.js
+++ assets/js/main.js
@@ -118,6 +118,14 @@ function scrollUp(){
 }
 window.addEventListener('scroll', scrollUp)
 
+/* CHANGE BACKGROUND HEADER */
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 /* DARK/LIGHT THEME */
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
