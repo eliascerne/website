4a064eff17ac85d11bbaca48f76a26c4205f81e0
erias04 <lichtbargeld@gmail.com>
2021-09-07 23:07:23 +0200

    feat: changed imports and set a port to listen Set a port (3000) to listen for the server, very basic. (Port is defined at the top). And changed the import from to const require :(
    I know from somewhere that import from is working with nodejs (I know it is working with python).

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-import express from "express"
-import bodyParser from "body-parser"
-import exphbs from "express-handlebars"
-import nodemailer from "nodemailer"
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer');
+
+const port = 3000
 
 const app = express();
 
 app.get('/', (req, res) =>{
     res.send('Hello');
 });
+
+app.listen(port, () => console.log('Server started at port: ', port));
