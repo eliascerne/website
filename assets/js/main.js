564e782ee944bb8e97cf9ddf5b4a40078cf66317
erias04 <lichtbargeld@gmail.com>
2021-11-06 13:00:05 +0100

    delete terminal from main because it is in beta and will be coded in develop

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,26 +157,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-
-/* Terminal CLI */
-// const terminalInput = document.getElementById('terminal_input');
-
-// terminalInput.addEventListener('keyup', e => {
-//     var scWidth = e.target.scrollWidth;
-//     terminalInput.style.width = `${scWidth}px`;
-
-//     var scHeight = e.target.scrollHeight;
-//     terminalInput.style.width = `${scWidth}px`;
-
-    
-//     console.log(scWidth);
-//     console.log(scHeight);
-// })
-
-const terminalInput = document.getElementById('terminal_input');
-const teriminalText = document.getElementById('terminal_command_text');
-
-terminalInput.addEventListener('input', function() {
-    teriminalText.innerHTML = terminalInput.value;
-    console.log(terminalInput.value);
-}, false);
