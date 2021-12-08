3d1bf9fb268767d1612309f7ac88c49afee59c8f
erias04 <lichtbargeld@gmail.com>
2021-12-08 11:54:34 +0100

    feat: added nav_logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Header() {
     return (
         <header className="header" id="header">
-            
+            <nav className="nav container">
+                <a href="#" className="nav_logo">Elias Cerne <p className="country_code">AT</p></a>
+            </nav>
         </header>
     )
 }
