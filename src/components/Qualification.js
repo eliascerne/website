7cd646c418edbb4ab166e08d8cc19ea8a6057a9e
erias04 <lichtbargeld@gmail.com>
2021-12-08 16:39:11 +0100

    feat: Qualification.js: changed class to className

--- src/components/Qualification.js
+++ src/components/Qualification.js
@@ -6,165 +6,165 @@ function Qualification() {
             <h2 className="section_title">Qualification</h2>
             <span className="section_subtitle">My personal journey</span>
 
-            <div class="qualification_container container">
-                <div class="qualification_tabs">
-                    <div class="qualification_button button--flex qualification_active" data-target='#education'>
-                        <i class="uil uil-graduation-cap qualification_icon"></i>
+            <div className="qualification_container container">
+                <div className="qualification_tabs">
+                    <div className="qualification_button button--flex qualification_active" data-target='#education'>
+                        <i className="uil uil-graduation-cap qualification_icon"></i>
                         Education
                     </div>
 
-                    <div class="qualification_button button--flex" data-target="#work">
-                        <i class="uil uil-briefcase-alt qualification_icon"></i>
+                    <div className="qualification_button button--flex" data-target="#work">
+                        <i className="uil uil-briefcase-alt qualification_icon"></i>
                         Work
                     </div>
 
-                    <div class="qualification_button button--flex" data-target="#fun">
-                        <i class="uil uil-web-grid qualification_icon"></i>
+                    <div className="qualification_button button--flex" data-target="#fun">
+                        <i className="uil uil-web-grid qualification_icon"></i>
                         Fun
                     </div>
                 </div>
 
-                <div class="qualification_sections">
+                <div className="qualification_sections">
                     {/* QUALIFICATION CONTENT Education */}
-                    <div class="qualification_content qualification_active" data-content id="education">
+                    <div className="qualification_content qualification_active" data-content id="education">
                         {/* QUALIFICATION 1 */}
-                        <div class="qualification_data">
+                        <div className="qualification_data">
                             <div>
-                                <h3 class="qualification_title">Elementary Scool</h3>
-                                <span class="qualification_subtitle">Volksschule II Neu Guntramsdorf</span>
-                                <div class="qualification_calendar">
-                                    <i class="uil uil-calender"></i>
+                                <h3 className="qualification_title">Elementary Scool</h3>
+                                <span className="qualification_subtitle">Volksschule II Neu Guntramsdorf</span>
+                                <div className="qualification_calendar">
+                                    <i className="uil uil-calender"></i>
                                     2010 - 2014
                                 </div>
                             </div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
                         </div>
 
                         {/* QUALIFICATION 2 */}
-                        <div class="qualification_data">
+                        <div className="qualification_data">
                             <div></div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
 
                             <div>
-                                <h3 class="qualification_title">Grammar School</h3>
-                                <span class="qualification_subtitle">BG/BRG Frauengasse</span>
-                                <div class="qualification_calendar">
-                                    <i class="uil uil-calender"></i>
+                                <h3 className="qualification_title">Grammar School</h3>
+                                <span className="qualification_subtitle">BG/BRG Frauengasse</span>
+                                <div className="qualification_calendar">
+                                    <i className="uil uil-calender"></i>
                                     2014- 2018
                                 </div>
                             </div>
                         </div>
 
                         {/* QUALIFICATION 3 */}
-                        <div class="qualification_data">
+                        <div className="qualification_data">
                             <div>
-                                <h3 class="qualification_title">ELectrical Engineering School</h3>
-                                <span class="qualification_subtitle">HTL-Mödling</span>
-                                <div class="qualification_calendar">
-                                    <i class="uil uil-calender"></i>
+                                <h3 className="qualification_title">ELectrical Engineering School</h3>
+                                <span className="qualification_subtitle">HTL-Mödling</span>
+                                <div className="qualification_calendar">
+                                    <i className="uil uil-calender"></i>
                                     2018 - now
                                 </div>
                             </div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                {/*<span class="qualification_line"></span>*/}
+                                <span className="qualification_rounder"></span>
+                                {/*<span className="qualification_line"></span>*/}
                             </div>
                         </div>
                     </div>
 
                     {/* QUALIFICATION CONTENT Work */}
-                    <div class="qualification_content" data-content id="work">
+                    <div className="qualification_content" data-content id="work">
                         {/* QUALIFICATION 1 */}
-                        <div class="qualification_data">
+                        <div className="qualification_data">
                             <div>
-                                <h3 class="qualification_title">Elementary Scool</h3>
-                                <span class="qualification_subtitle">Volksschule II Neu Guntramsdorf</span>
-                                <div class="qualification_calendar">
-                                    <i class="uil uil-calender"></i>
+                                <h3 className="qualification_title">Elementary Scool</h3>
+                                <span className="qualification_subtitle">Volksschule II Neu Guntramsdorf</span>
+                                <div className="qualification_calendar">
+                                    <i className="uil uil-calender"></i>
                                     2010 - 2014
                                 </div>
                             </div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
                         </div>
 
                         {/* QUALIFICATION 2 */}
-                        <div class="qualification_data">
+                        <div className="qualification_data">
                             <div></div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
 
                             <div>
-                                <h3 class="qualification_title">Grammar School</h3>
-                                <span class="qualification_subtitle">BG/BRG Frauengasse</span>
-                                <div class="qualification_calendar">
-                                    <i class="uil uil-calender"></i>
+                                <h3 className="qualification_title">Grammar School</h3>
+                                <span className="qualification_subtitle">BG/BRG Frauengasse</span>
+                                <div className="qualification_calendar">
+                                    <i className="uil uil-calender"></i>
                                     2014- 2018
                                 </div>
                             </div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
                         </div>
                     </div>
 
                     {/* QUALIFICATION CONTENT Fun */}
-                    <div class="qualification_content" data-content id="fun">
+                    <div className="qualification_content" data-content id="fun">
                         {/* QUALIFICATION 1 */}
-                        <div class="qualification_data">
+                        <div className="qualification_data">
                             <div>
-                                <h3 class="qualification_title">Elementary Scool</h3>
-                                <span class="qualification_subtitle">Volksschule II Neu Guntramsdorf</span>
-                                <div class="qualification_calendar">
-                                    <i class="uil uil-calender"></i>
+                                <h3 className="qualification_title">Elementary Scool</h3>
+                                <span className="qualification_subtitle">Volksschule II Neu Guntramsdorf</span>
+                                <div className="qualification_calendar">
+                                    <i className="uil uil-calender"></i>
                                     2010 - 2014
                                 </div>
                             </div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
                         </div>
 
                         {/* QUALIFICATION 2 */}
-                        <div class="qualification_data">
+                        <div className="qualification_data">
                             <div></div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
 
                             <div>
-                                <h3 class="qualification_title">Grammar School</h3>
-                                <span class="qualification_subtitle">BG/BRG Frauengasse</span>
-                                <div class="qualification_calendar">
-                                    <i class="uil uil-calender"></i>
+                                <h3 className="qualification_title">Grammar School</h3>
+                                <span className="qualification_subtitle">BG/BRG Frauengasse</span>
+                                <div className="qualification_calendar">
+                                    <i className="uil uil-calender"></i>
                                     2014- 2018
                                 </div>
                             </div>
 
                             <div>
-                                <span class="qualification_rounder"></span>
-                                <span class="qualification_line"></span>
+                                <span className="qualification_rounder"></span>
+                                <span className="qualification_line"></span>
                             </div>
                         </div>
                     </div>
