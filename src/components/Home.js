517618989d4ae9718b7ad18f230ffdfed021bfdb
erias04 <lichtbargeld@gmail.com>
2021-12-08 13:52:27 +0100

    feat: added whole Home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { UilInstagram, UilGithubAlt } from '@iconscout/react-unicons'
+import { UilInstagram, UilGithubAlt, UilMessage, UilAngleDown } from '@iconscout/react-unicons'
+
 
 function Home() {
     return (
@@ -17,17 +18,26 @@ function Home() {
                     </div>
 
                     <div className="home_img">
-                        <svg className="home_blob" viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                            <g mask="url(#mask0)">
-                                <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346
-                                165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403
-                                129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028
-                                -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
-
-                                <image className="home_blob-img" x='12' y='18' xlinkHref="" />
-                            </g>
-                        </svg>
+                        <img className="home_blob-img" src="" alt="Elias Cerne, EliasCerne, eliascerne, elias cerne, cerne, Cerne, Elias, elias" />
                     </div>
+
+                    <div className="home_data">
+                        <h1 className="home_title">Hey, I'm Elias</h1>
+                        <h3 className="home_subtitle">Student</h3>
+                        <p className="home_description">A programming enthusiastic Student at HTL Mödling.</p>
+
+                        <a href="#contact" className="button button--flex">
+                            Contact Me
+                            <UilMessage className="button_icon" size="20" />
+                        </a>
+                    </div>
+                </div>
+
+                <div className="home_scroll">
+                    <a href="#about" className="home_scroll-button button--flex">
+                        <UilAngleDown size="20" className="home_scroll-arrow" />
+                        <span className="home_scroll-name">Scroll down</span>
+                    </a>
                 </div>
             </div>
         </section>
