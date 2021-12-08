32a52e551fde80a2e5ac3dac20b0f239377176b2
erias04 <lichtbargeld@gmail.com>
2021-12-08 11:52:39 +0100

    create: Header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Header />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="header" id="header">
+            
+        </header>
+    )
+}
+
+export default Header
