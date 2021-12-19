7f29ce19051e4ca2bae82f53fcd2605b63558a2a
erias04 <lichtbargeld@gmail.com>
2021-12-19 18:21:57 +0100

    feat: added geogocation api to fetch language

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import IPGeolocationAPI from 'ip-geolocation-api-javascript-sdk';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -12,6 +13,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Terminal from './components/Terminal';
 import ScrollUp from './components/ScrollUp';
+import BinaryClock from './components/BinaryClock';
 
 function App() {
 
@@ -19,14 +21,16 @@ function App() {
 
   const [showNavMenu, setShowNavMenu] = useState(false);
   const [selectedTheme, setSelectedTheme] = useState('');
+  const [language, setLanguage] = useState('english');
+  const [loading, setLoading] = useState(true);
 
-  
-  useEffect(() => {
+
+  useLayoutEffect(() => {
     const selectedTheme = localStorage.getItem('selectedTheme');
 
     if (selectedTheme) {
       selectedTheme === 'dark' ? setSelectedTheme('dark') : setSelectedTheme('white')
-    } 
+    }
     else {
       let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
       if (matched) {
@@ -34,11 +38,38 @@ function App() {
         localStorage.setItem('selectedTheme', 'dark')
       }
     }
+    setLoading(false);
+
+    var ipgeolocationApi = new IPGeolocationAPI('2c3a4ea379de4c2b8c7e3f23e3d664dd', false);
+    ipgeolocationApi.getGeolocation(handleResponse);
+    function handleResponse(json) {
+      console.log(json.country_code2);
+      if (json.country_code2 === 'AT' | 'DE') {
+        console.log(1);
+        setLanguage('german');
+        
+      }
+    }
+
   }, []);
 
+  useEffect(() => {
+    }, []);
+
+
+
+  // if (language === 'german') {
+  //   return null
+  // }
 
   return (
-    <body className={selectedTheme === 'dark' ? 'dark-theme' : undefined}>
+    loading ? 
+      <body className="dark-theme">
+        <BinaryClock />
+      </body>
+
+      :
+      <body className={selectedTheme === 'dark' ? 'dark-theme' : undefined}>
       <Header showNavMenu={showNavMenu} setShowNavMenu={setShowNavMenu} selectedTheme={selectedTheme} setSelectedTheme={setSelectedTheme} />
       <main className="main" onClick={showNavMenu ? () => setShowNavMenu(false) : undefined}>
         <Home />
