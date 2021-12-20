f92a35bb707a5a0d932a8fcfd9e29c846daca76d
erias04 <lichtbargeld@gmail.com>
2021-12-20 11:48:57 +0100

    feat: changed text to Data.js (different languages)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { UilEstate, UilUser, UilFileAlt, UilBag, UilTimes, UilMoon, UilSun, UilApps } from '@iconscout/react-unicons'
+import Data from '../Data.json'
 
-function Header({ showNavMenu, setShowNavMenu, selectedTheme, setSelectedTheme }) {
+function Header({ showNavMenu, setShowNavMenu, selectedTheme, setSelectedTheme, languageJSON }) {
 
     const [darkerHeader, setDarkerHeader] = useState(false);
     // const [IconTheme, setIconTheme] = useState('UilSun');
@@ -37,43 +38,43 @@ function Header({ showNavMenu, setShowNavMenu, selectedTheme, setSelectedTheme }
     return (
         <header className={"header " + (darkerHeader ? 'scroll-header' : undefined)} id="header">
             <nav className="nav container">
-                <a href="/" className="nav_logo">Elias Cerne <p className="country_code">AT</p></a>
+                <a href="/" className="nav_logo">{Data.language[languageJSON].header.heading} <p className="country_code">AT</p></a>
 
                 <div className={"nav_menu " + (showNavMenu ? "show-menu" : "")} id="nav-menu">
                     <ul className="nav_list grid" onClick={toggleMenu}>
                         <li className="nav_item">
                             <a href="#home" className="nav_link active-link">
-                                <UilEstate size="19.2" className="nav_icon" /> Home
+                                <UilEstate size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink1}
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#about" className="nav_link">
-                                <UilUser size="19.2" className="nav_icon" /> About
+                                <UilUser size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink2}
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#skills" className="nav_link">
-                                <UilFileAlt size="19.2" className="nav_icon" /> Skills
+                                <UilFileAlt size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink3}
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#qualification" className="nav_link">
-                                <UilBag size="19.2" className="nav_icon" /> Qualification
+                                <UilBag size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink4}
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#services" className="nav_link">
-                                <UilBag size="19.2" className="nav_icon" /> Services
+                                <UilBag size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink5}
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#contact" className="nav_link">
-                                <UilUser size="19.2" className="nav_icon" /> Contact me
+                                <UilUser size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink6}
                             </a>
                         </li>
                     </ul>
