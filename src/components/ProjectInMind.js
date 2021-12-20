33fa96605ed0f5459fb26486dae4347d4e2f8377
erias04 <lichtbargeld@gmail.com>
2021-12-20 01:02:06 +0100

    feat: changed text to Data.js (different languages)

--- src/components/ProjectInMind.js
+++ src/components/ProjectInMind.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { UilMessage } from '@iconscout/react-unicons'
+import Data from '../Data.json'
 
-function ProjectInMind() {
+function ProjectInMind({ languageJSON }) {
     return (
         <section className="project section" id="project-in-mind">
             <div className="project_bg">
                 <div className="project_container container grid">
                     <div className="project_data">
-                        <h2 className="project_title">You have a new project</h2>
-                        <p className="project_description">Contact Me now!</p>
+                        <h2 className="project_title">{Data.language[languageJSON].projectInMind.heading}</h2>
+                        <p className="project_description">{Data.language[languageJSON].projectInMind.description}</p>
                         <a href="#contact" className="button button--flex button--white">
-                            Contact Me
+                        {Data.language[languageJSON].projectInMind.button}
                             <UilMessage className="project_icon button_icon" size="20" />
                         </a>
                     </div>
