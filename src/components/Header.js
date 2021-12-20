1cd419d31774fed4730f02f57379d297bd33d394
erias04 <lichtbargeld@gmail.com>
2021-12-20 18:27:44 +0100

    feat: implement Language Switcher in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { UilEstate, UilUser, UilFileAlt, UilBag, UilTimes, UilMoon, UilSun, UilApps } from '@iconscout/react-unicons'
-import Data from '../Data.json'
 
-function Header({ showNavMenu, setShowNavMenu, selectedTheme, setSelectedTheme, languageJSON, setLanguageJSON, language, setLanguage }) {
+function Header({ showNavMenu, setShowNavMenu, selectedTheme, setSelectedTheme }) {
 
     const [darkerHeader, setDarkerHeader] = useState(false);
-    const [languageSwitch, setLanguageSwitch] = useState(false);
+    // const [IconTheme, setIconTheme] = useState('UilSun');
 
     function toggleMenu() {
         setShowNavMenu(!showNavMenu);
@@ -35,82 +34,46 @@ function Header({ showNavMenu, setShowNavMenu, selectedTheme, setSelectedTheme, 
         }
     }
 
-    function showLanguageSwitch() {
-        setLanguageSwitch(true);
-    }
-
-    function hideLanguageSwitch() {
-        setLanguageSwitch(false);
-    }
-
-    function changeLanguageToEN() {
-        setLanguageSwitch(false);
-        setLanguage('en')
-        setLanguageJSON(0);
-        localStorage.setItem('selectedLanguage', 'en')
-    }
-
-    function changeLanguageToDE() {
-        setLanguageSwitch(false);
-        setLanguage(')
-        setLanguageJSON(1);
-        localStorage.setItem('selectedLanguage', 'de')
-    }
-
     return (
         <header className={"header " + (darkerHeader ? 'scroll-header' : undefined)} id="header">
             <nav className="nav container">
-                <a href="#home" className="nav_logo" onMouseOver={() => showLanguageSwitch()}>{Data.language[languageJSON].header.heading} <p className="country_code">{language}</p></a>
-                <div className={"language_switch_container " + (languageSwitch ? undefined : "hide")} onMouseLeave={() => hideLanguageSwitch()}>
-                    <div className="language_switch_box">
-                        <div className="language_switch_content">
-                            <div className="language_en" onClick={() => changeLanguageToEN()}>
-                                <img src="/images/FlagEngland.png" width="50px" height="50px" alt="" className="language_flag"></img>
-                                <div className="language_title">English</div>
-                            </div>
-                            <div className="language_de" onClick={() => changeLanguageToDE()}>
-                                <img src="/images/FlagAustria.png" width="50px" height="50px" alt="" className="language_flag"></img>
-                                <div className="language_title">German</div>  
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <a href="/" className="nav_logo">Elias Cerne <p className="country_code">AT</p></a>
 
                 <div className={"nav_menu " + (showNavMenu ? "show-menu" : "")} id="nav-menu">
                     <ul className="nav_list grid" onClick={toggleMenu}>
                         <li className="nav_item">
                             <a href="#home" className="nav_link">
-                                <UilEstate size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink1}
+                                <UilEstate size="19.2" className="nav_icon" /> Home
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#about" className="nav_link">
-                                <UilUser size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink2}
+                                <UilUser size="19.2" className="nav_icon" /> About
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#skills" className="nav_link">
-                                <UilFileAlt size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink3}
+                                <UilFileAlt size="19.2" className="nav_icon" /> Skills
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#qualification" className="nav_link">
-                                <UilBag size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink4}
+                                <UilBag size="19.2" className="nav_icon" /> Qualification
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#services" className="nav_link">
-                                <UilBag size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink5}
+                                <UilBag size="19.2" className="nav_icon" /> Services
                             </a>
                         </li>
 
                         <li className="nav_item">
                             <a href="#contact" className="nav_link">
-                                <UilUser size="19.2" className="nav_icon" /> {Data.language[languageJSON].header.navLink6}
+                                <UilUser size="19.2" className="nav_icon" /> Contact me
                             </a>
                         </li>
                     </ul>
