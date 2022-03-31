226840f715b45adc154700777b108c1ef0949829
erias04 <github.eliascerne@icloud.com>
2022-03-31 13:21:04 +0200

    created: ProjectInMind lib

---
+++ libs/section/project-in-mind/README.md
@@ -0,0 +1,7 @@
+# section-project-in-mind
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test section-project-in-mind` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/section/project-in-mind/src/lib/SectionProjectInMind.tsx
@@ -0,0 +1,14 @@
+import styles from './SectionProjectInMind.module.css';
+
+/* eslint-disable-next-line */
+export interface SectionProjectInMindProps {}
+
+export function SectionProjectInMind(props: SectionProjectInMindProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SectionProjectInMind!</h1>
+    </div>
+  );
+}
+
+export default SectionProjectInMind;
