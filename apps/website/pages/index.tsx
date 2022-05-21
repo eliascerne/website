87fdb2d8eb02eafae05e736b1bc68d7901508c15
erias04 <github.eliascerne@icloud.com>
2022-05-21 22:39:55 +0200

    feat: added image path

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -8,9 +8,9 @@ import styles from './index.module.css';
 export function Home() {
   return (
     <div className="grid grid-flow-row-dense grid-cols-1 grid-rows-1 gap-4 pt-6 md:grid-cols-3 lg:grid-cols-4">
-      <CardsUi size="medium" type="about" image="" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook. Oh and I can speak JS, TS, JS and more Typescript." />
-      <CardsUi size="small" type="about" image="" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
-      <CardsUi size="small" type="about" image="" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
+      <CardsUi size="medium" type="about" image="https://r2.eriascdn.com/erias.png" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook. Oh and I can speak JS, TS, JS and more Typescript." />
+      <CardsUi size="small" type="about" image="https://r2.eriascdn.com/erias.png" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
+      <CardsUi size="small" type="about" image="https://r2.eriascdn.com/erias.png" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
     </div>
   );
 }
