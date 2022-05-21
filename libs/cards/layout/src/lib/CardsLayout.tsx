fe761f474bf46b60366b5b5bbf55f650e564cca3
erias04 <github.eliascerne@icloud.com>
2022-05-21 22:32:16 +0200

    feat: styled CardsLayout

--- libs/cards/layout/src/lib/CardsLayout.tsx
+++ libs/cards/layout/src/lib/CardsLayout.tsx
@@ -1,12 +1,33 @@
+import { ReactChild } from 'react';
 import styles from './CardsLayout.module.css';
 
+const sizeMap: any = {
+  small: 1,
+  medium: 2,
+  large: 3,
+  xlarge: 4,
+};
+
 /* eslint-disable-next-line */
-export interface CardsLayoutProps {}
+export interface CardsLayoutProps {
+  size: string;
+  children: ReactChild;
+}
 
 export function CardsLayout(props: CardsLayoutProps) {
+  const { size, children } = props;
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to CardsLayout!</h1>
+    <div
+      className={
+        'bg-card border-4 border-cardBorder rounded-3xl text-white h-full min-h-[278px] w-full hover:border-cardBorderHover transition-all duration-300 ease-in-out row-span-1' +
+        ' ' +
+        'col-span-' +
+        sizeMap[size]
+      }
+    >
+      {children}
+      <div className="col-span-1 col-span-2 col-span-3 col-span-4"></div>
     </div>
   );
 }
