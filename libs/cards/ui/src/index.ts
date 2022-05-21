11e636c64e7f24e0183bb2d2f03f8ac3154fe4a6
erias04 <github.eliascerne@icloud.com>
2022-05-21 19:32:30 +0200

    feat: added typings for the Cards UI endpoint

--- libs/cards/ui/src/index.ts
+++ libs/cards/ui/src/index.ts
@@ -1,1 +1,28 @@
+type cardSize = 'small' | 'medium' | 'large';
+
+export interface baseCard {
+  size: cardSize;
+  background?: string;
+  borderColor?: string;
+  borderThic?: string | number;
+}
+
+export interface aboutCard extends baseCard {
+  type: 'about';
+  image: any;
+  description: string;
+}
+
+export interface linkCard extends baseCard {
+  type: 'link';
+  image: any;
+  description: string;
+  buttonIcon: any;
+  buttonText: string;
+}
+
+type Cards = aboutCard | linkCard;
+
+export default Cards;
+
 export * from './lib/CardsUi';
