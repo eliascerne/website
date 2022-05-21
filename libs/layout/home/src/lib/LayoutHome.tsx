7991c782f995346c7380caae63f497e45e783631
erias04 <github.eliascerne@icloud.com>
2022-05-21 17:57:32 +0200

    feat: changed LayoutHome content

--- libs/layout/home/src/lib/LayoutHome.tsx
+++ libs/layout/home/src/lib/LayoutHome.tsx
@@ -1,21 +1,10 @@
-import { ReactChild } from 'react';
-import { UtilsHeader } from '@eliascerne/utils/header';
-
 import styles from './LayoutHome.module.css';
 
 /* eslint-disable-next-line */
-export interface LayoutHomeProps {
-  children: ReactChild;
-}
+export interface LayoutHomeProps {}
 
 export function LayoutHome(props: LayoutHomeProps) {
-  return (
-      <div className="md:mt-header px-6 max-w-maxWidth mx-auto xl:px-0">
-        <UtilsHeader />
-        <h1 className="text-white">Welcome to LayoutHome!</h1>
-        {props.children}
-      </div>
-  );
+  return <h1 className="text-white">Welcome to LayoutHome!</h1>;
 }
 
 export default LayoutHome;
