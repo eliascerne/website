47ae2d630e97bee02e12565b3f3d58e25a5ac7ea
erias04 <github.eliascerne@icloud.com>
2022-05-21 16:30:27 +0200

    feat: added swiping animation functionality

--- libs/utils/header/src/lib/list/list.tsx
+++ libs/utils/header/src/lib/list/list.tsx
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router';
-import { useState, useRef } from 'react';
+import { useState, useRef, useCallback, useEffect } from 'react';
+import { AnimateSharedLayout } from 'framer-motion';
+import { ListItem } from '@eliascerne/utils/header';
+import Link from 'next/link';
 
 import styles from './list.module.css';
 
@@ -9,41 +12,91 @@ export interface ListProps {}
 const menuItems = [
   {
     index: 0,
-    to: '/',
+    href: '/',
     name: 'Home',
   },
   {
     index: 1,
-    to: '/about',
+    href: '/about',
     name: 'About',
   },
   {
     index: 2,
-    to: '/work',
+    href: '/work',
     name: 'Work',
   },
   {
     index: 3,
-    to: '/blog',
+    href: '/blog',
     name: 'Blog',
   },
 ];
 
 function getActiveIndex(pathname: any) {
-  return menuItems.findIndex((item) => item.to === pathname);
+  return menuItems.findIndex((item) => item.href === pathname);
 }
 
 export function List(props: ListProps) {
   const { route } = useRouter();
-  const [activeHoverIndex, setActiveHoverIndex] = useState(() =>
+  const [activeHoverIndex, setActiveHoverIndex] = useState<any>(() =>
     getActiveIndex(route)
   );
-  const hasClicked = useRef(false);
-  const hasHover = useRef(false);
+  const isClicked = useRef<boolean>(false);
+  const isHover = useRef<boolean>(false);
+
+  const handleOnClicked = () => {
+    isClicked.current = true;
+  };
+
+  const handleOnHoverStart = useCallback(
+    (index) => () => {
+      isHover.current = true;
+      setActiveHoverIndex(index);
+    },
+    []
+  );
+
+  function handleOnHoverEnd() {
+    if (isClicked.current) {
+      return;
+    }
+    const index = getActiveIndex(route);
+
+    if (index < 0) {
+      setTimeout(() => {
+        if (isHover.current) return;
+        setActiveHoverIndex(index);
+      }, 500);
+    } else {
+      setActiveHoverIndex(index);
+    }
+
+    isHover.current = false;
+  }
+
+  useEffect(() => {
+    isClicked.current = false;
+    setActiveHoverIndex(getActiveIndex(route));
+  }, [route]);
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to List!</h1>
-    </div>
+    <AnimateSharedLayout>
+      <ul className="flex items-center justify-center flex-none w-full place-items-center md:w-auto">
+        {menuItems.map(({ href, name, index }) => (
+          <ListItem
+            key={index}
+            active={activeHoverIndex === index}
+            onHoverStart={handleOnHoverStart(index)}
+            onHoverEnd={handleOnHoverEnd}
+            onClick={handleOnClicked}
+          >
+            <Link href={href}>
+              <p className="relative z-20 cursor-pointer px-4 lg:px-6">{name}</p>
+            </Link>
+          </ListItem>
+        ))}
+      </ul>
+    </AnimateSharedLayout>
   );
 }
 
