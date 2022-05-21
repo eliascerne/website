8279d01f613fd18a6fdfcbf431d2c6911be4d38e
erias04 <github.eliascerne@icloud.com>
2022-05-21 14:00:40 +0200

    feat: added new components and max-w as well as center

--- libs/utils/header/src/lib/UtilsHeader.tsx
+++ libs/utils/header/src/lib/UtilsHeader.tsx
@@ -13,8 +13,8 @@ export interface UtilsHeaderProps {}
 export function UtilsHeader(props: UtilsHeaderProps) {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <header className="fixed w-full bottom-0 left-0 py-2 z-40 bg-background">
-      <nav className="max-w-header ml-6 mr-5 h-header flex justify-between items-center text-white">
+    <header className="fixed w-full max-w-maxWidth bottom-0 left-0 py-2 z-40 bg-background md:top-0 md:max-h-header md:py-0 md:left-1/2 md:-translate-x-1/2">
+      <nav className="ml-6 mr-5 flex justify-between items-center text-white md:h-header xl:ml-0 xl:mr-0">
         <Link href="/">
           <div className="cursor-pointer flex gap-1.5 items-center">
             <div className="w-7 text-xs text-white">
@@ -26,9 +26,33 @@ export function UtilsHeader(props: UtilsHeaderProps) {
             </div>
           </div>
         </Link>
+        <div className="hidden md:block font-extrabold">
+          <ul className="flex gap-10">
+            <li className="cursor-pointer">
+              <Link href="/">
+                <p className="">Home</p>
+              </Link>
+            </li>
+            <li className="cursor-pointer">
+              <Link href="/about">
+                <p className="">About</p>
+              </Link>
+            </li>
+            <li className="cursor-pointer">
+              <Link href="/work">
+                <p className="">Work</p>
+              </Link>
+            </li>
+            <li className="cursor-pointer">
+              <Link href="/blog">
+                <p className="">Blog</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
         <div className="flex gap-3 items-center cursor-pointer">
           <Link href="/">
-            <div className="px-2.5 py-1 border-[1px] border-white rounded-3xl text-sm font-semibold">
+            <div className="px-2.5 py-1 border-[1px] border-white rounded-3xl text-sm font-semibold ">
               Open Chat
             </div>
           </Link>
@@ -38,7 +62,7 @@ export function UtilsHeader(props: UtilsHeaderProps) {
               // onClick={() => setNavMenuOpen(true)}
             />
             <UilApps
-              className="w-5"
+              className="w-5 md:hidden"
               onClick={() => setMenuOpen(true)}
             />
           </div>
