b98907dfd8ca05e3fc8c3cbee0ef80633e2b4ef4
erias04 <github.eliascerne@icloud.com>
2022-05-21 22:40:21 +0200

    feat: added image props to CardsUi

--- libs/cards/ui/src/lib/CardsUi.tsx
+++ libs/cards/ui/src/lib/CardsUi.tsx
@@ -3,13 +3,13 @@ import Cards from '@eliascerne/cards/ui';
 import { CardsAbout } from '@eliascerne/cards/about';
 
 /* eslint-disable-next-line */
-export interface CardsUiProps {}
+// export interface CardsUiProps {}
 
 export function CardsUi(props: Cards) {
-  const { size, description } = props;
+  const { size, description, image } = props;
   if (!('type' in props)) return;
 
-  if (props.type === 'about') return <CardsAbout size={size} description={description} />;
+  if (props.type === 'about') return <CardsAbout size={size} description={description} image={image} />;
 
   return (
     <div className={styles['container']}>
