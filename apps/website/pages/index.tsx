cbe472fd52a3a89484e7d1ca54fd6384e563e779
erias04 <github.eliascerne@icloud.com>
2022-05-21 22:31:56 +0200

    feat: added a couple of elements

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -1,41 +1,16 @@
 import type { ReactElement } from 'react';
 import { LayoutBase } from '@eliascerne/layout/base';
 import { CardsUi } from '@eliascerne/cards/ui';
+import { CardsLayout } from '@eliascerne/cards/layout';
 
 import styles from './index.module.css';
 
 export function Home() {
   return (
-    <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-3">
-      <div className="bg-slate-800">01</div>
-      <div className="bg-amber-200">02</div>
-      <div className="col-span-2 bg-slate-50">03</div>
-      <div className="bg-slate-800">04</div>
-      <div className="bg-amber-200">05</div>
-      <div className="col-span-2 bg-slate-400">06</div>
-      <div className="bg-amber-700">07</div>
-      <div className="bg-amber-700">08</div>
-      <div className="bg-amber-700">09</div>
-      <div className="col-span-2 bg-slate-50">10</div>
-      <div className="bg-slate-800">11</div>
-      <div className="col-span-2 bg-slate-400">12</div>
-      <div className="col-span-2 bg-slate-400">12</div>
-      <div className="col-span-2 bg-slate-400">12</div>
-      <div className="col-span-2 bg-slate-400">12</div>
-      <div className="col-span-2 bg-slate-400">12</div>
-      <div className="col-span-2 bg-slate-400">12</div>
-      <div className="bg-amber-700">13</div>
-      <div className="bg-amber-700">14</div>
-      <div className="col-span-2 bg-slate-50">15</div>
-      <div className="col-span-2 bg-slate-400">16</div>
-      <div className="bg-amber-700">17</div>
-      <div className="bg-amber-200">18</div>
-      <CardsUi
-        size="medium"
-        type="about"
-        image=""
-        description=""
-      />
+    <div className="grid grid-flow-row-dense grid-cols-1 grid-rows-1 gap-4 pt-6 md:grid-cols-3 lg:grid-cols-4">
+      <CardsUi size="medium" type="about" image="" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook. Oh and I can speak JS, TS, JS and more Typescript." />
+      <CardsUi size="small" type="about" image="" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
+      <CardsUi size="small" type="about" image="" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
     </div>
   );
 }
