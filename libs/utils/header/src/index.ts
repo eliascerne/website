d00589d6f2b150669609d3d6a182005ea7d30b32
erias04 <github.eliascerne@icloud.com>
2022-05-21 16:29:57 +0200

    feat: added new exports from header

--- libs/utils/header/src/index.ts
+++ libs/utils/header/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/UtilsHeader';
 export * from './lib/Nav/Nav';
 export * from './lib/ListItem/ListItem';
-export * from './lib/ListHover/ListHover'
+export * from './lib/ListHover/ListHover';
+export * from './lib/List/List';
