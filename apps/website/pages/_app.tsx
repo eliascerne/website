9ddff6bda974e3fbe322a0896dcc49c76f051664
erias04 <github.eliascerne@icloud.com>
2022-05-21 09:35:01 +0200

    feat: changed title

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to website!</title>
+        <title>Elias Cerne</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
