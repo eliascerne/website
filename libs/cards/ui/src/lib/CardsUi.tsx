03385b57179b0abc2e4b226126857a6270bc622e
erias04 <github.eliascerne@icloud.com>
2022-05-21 19:32:48 +0200

    feat: check for no type props when calling Cards

--- libs/cards/ui/src/lib/CardsUi.tsx
+++ libs/cards/ui/src/lib/CardsUi.tsx
@@ -1,9 +1,13 @@
 import styles from './CardsUi.module.css';
+import Cards from '@eliascerne/cards/ui';
+import { CardsLayout } from '@eliascerne/cards/layout';
 
 /* eslint-disable-next-line */
 export interface CardsUiProps {}
 
-export function CardsUi(props: CardsUiProps) {
+export function CardsUi(props: Cards) {
+  if (!('type' in props)) return;
+
   return (
     <div className={styles['container']}>
       <h1>Welcome to CardsUi!</h1>
