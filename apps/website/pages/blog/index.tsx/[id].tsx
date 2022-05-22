8a26a40816d4a02b8b3a1922e9c4560c7b51ad6e
erias04 <github.eliascerne@icloud.com>
2022-05-22 13:07:12 +0200

    feat: added new dynamic url in blog

---
+++ apps/website/pages/blog/index.tsx/[id].tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface IndexTsxProps {}
+
+export function IndexTsx(props: IndexTsxProps) {
+  return (
+    <div>
+      <h1>Welcome to IndexTsx!</h1>
+    </div>
+  );
+}
+
+export default IndexTsx;
