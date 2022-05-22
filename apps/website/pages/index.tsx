c03330b7f2f7fa0e449af22329ecc5b7fac415b7
erias04 <github.eliascerne@icloud.com>
2022-05-22 12:35:27 +0200

    feat: added new cards

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -8,9 +8,33 @@ import styles from './index.module.css';
 
 export function Home() {
   return (
-    <div className="grid grid-flow-row grid-cols-1 grid-rows-3 gap-4 pt-6 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid grid-flow-row-dense grid-cols-1 grid-rows-3 gap-4 pt-6 md:grid-cols-3 lg:grid-cols-4">
+      <CardsUi
+        size="medium"
+        type="about"
+        image="https://r2.eriascdn.com/erias.png"
+        description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook."
+      />
       <CardsUi
         size="small"
+        type="link"
+        image="https://upload.wikimedia.org/wikipedia/commons/9/95/Tailwind_CSS_logo.svg"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
+        size="small"
+        type="link"
+        image="https://upload.wikimedia.org/wikipedia/commons/9/95/Tailwind_CSS_logo.svg"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
+        size="medium"
         type="blog"
         image="https://cdn.pocket-lint.com/r/s/970x/assets/images/158839-laptops-review-apple-macbook-pro-14-inch-review-image2-ribxgzs9jt-jpg.webp"
         title="Why serverless is the way to go"
@@ -22,9 +46,21 @@ export function Home() {
       />
       <CardsUi
         size="small"
-        type="about"
-        image="https://r2.eriascdn.com/erias.png"
-        description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook."
+        type="link"
+        image="https://upload.wikimedia.org/wikipedia/commons/9/95/Tailwind_CSS_logo.svg"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
+        size="small"
+        type="link"
+        image="https://upload.wikimedia.org/wikipedia/commons/9/95/Tailwind_CSS_logo.svg"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
       />
       <CardsUi
         size="small"
