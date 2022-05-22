3d985e2efdda5b702c7eb453b7fececb3209cb2b
erias04 <github.eliascerne@icloud.com>
2022-05-22 15:30:10 +0200

    feat: generated new editorjs insatnce

--- libs/editor/ui/src/lib/EditorUi.tsx
+++ libs/editor/ui/src/lib/EditorUi.tsx
@@ -1,12 +1,37 @@
+import { createReactEditorJS } from 'react-editor-js';
+import { EditorConfig } from '@eliascerne/editor/ui';
+import dynamic from 'next/dynamic';
+
 import styles from './EditorUi.module.css';
+
+const EditorJs = dynamic(() => import('react-editor-js') as any, {
+  ssr: false,
+});
 
 /* eslint-disable-next-line */
 export interface EditorUiProps {}
 
+const ReactEditorJS = createReactEditorJS();
+
 export function EditorUi(props: EditorUiProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to EditorUi!</h1>
+    <div className="text-white">
+      <ReactEditorJS
+        holder="editor"
+        // tools={EditorConfig}
+        defaultValue={{
+          time: 1635603431943,
+          blocks: [
+            {
+              id: '12iM3lqzcm',
+              type: 'paragraph',
+              data: {
+                text: 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text.',
+              },
+            },
+          ],
+        }}
+      />
     </div>
   );
 }
