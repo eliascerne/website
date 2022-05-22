1eaa07fa490efab14fdb670b6b0050bb72258ee3
erias04 <github.eliascerne@icloud.com>
2022-05-22 11:31:43 +0200

    feat: added two cards to main screen

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -2,15 +2,27 @@ import type { ReactElement } from 'react';
 import { LayoutBase } from '@eliascerne/layout/base';
 import { CardsUi } from '@eliascerne/cards/ui';
 import { CardsLayout } from '@eliascerne/cards/layout';
+import { UilAirplay } from '@iconscout/react-unicons';
 
 import styles from './index.module.css';
 
 export function Home() {
   return (
-    <div className="grid grid-flow-row-dense grid-cols-1 grid-rows-1 gap-4 pt-6 md:grid-cols-3 lg:grid-cols-4">
-      <CardsUi size="medium" type="about" image="https://r2.eriascdn.com/erias.png" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook. Oh and I can speak JS, TS, JS and more Typescript." />
-      <CardsUi size="small" type="about" image="https://r2.eriascdn.com/erias.png" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
-      <CardsUi size="small" type="about" image="https://r2.eriascdn.com/erias.png" description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook." />
+    <div className="grid grid-flow-row-dense grid-cols-1 grid-rows-3 gap-4 pt-6 md:grid-cols-3 lg:grid-cols-4">
+      <CardsUi
+        size="medium"
+        type="about"
+        image="https://r2.eriascdn.com/erias.png"
+        description="Hello👋 I'm Elias, an Austrian student and Web Developer. I work with Next.js, Tailwind, Figma and a MacBook."
+      />
+      <CardsUi
+        size="small"
+        type="link"
+        image="https://upload.wikimedia.org/wikipedia/commons/9/95/Tailwind_CSS_logo.svg"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        buttonText="Visit"
+        link="https://tailwindcss.com"
+      />
     </div>
   );
 }
