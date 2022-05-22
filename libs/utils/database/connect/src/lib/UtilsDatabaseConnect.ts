3863593e8c83e9f147fca8ca1bb17fb7b8da7ca4
erias04 <github.eliascerne@icloud.com>
2022-05-22 18:31:32 +0200

    feat: changed connect to Database varaible

--- libs/utils/database/connect/src/lib/UtilsDatabaseConnect.ts
+++ libs/utils/database/connect/src/lib/UtilsDatabaseConnect.ts
@@ -1,10 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-async function connectToDatabase(database: string = 'blog') {
+export async function UtilsDatabaseConnect(database: string = 'blog') {
   const client = await MongoClient.connect(
-    `mongodb+srv://erias:1wu6tYAdLxvwVcCG@cluster0.f7bqi.mongodb.net/${database}?retryWrites=true&w=majority`
+    `mongodb+srv://erias:1wu6tYAdLxvwVcCG@blog0.eu4te.mongodb.net/?retryWrites=true&w=majority`
   );
   return client;
 }
 
-export default connectToDatabase;
+export default UtilsDatabaseConnect;
