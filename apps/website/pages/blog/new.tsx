1a8584b1217475f51b6fb0d4c158bf7b3e2488fa
erias04 <github.eliascerne@icloud.com>
2022-05-22 13:49:02 +0200

    feat: added new page new.tsx in blog

---
+++ apps/website/pages/blog/new.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface IndexTsxProps {}
+
+export function BlogNew(props: IndexTsxProps) {
+  return (
+    <div>
+      <h1>Welcome to BlogNew!</h1>
+    </div>
+  );
+}
+
+export default BlogNew;
