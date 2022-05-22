c1d0c14c0248b1718118948134f8528e6e5a6f03
erias04 <github.eliascerne@icloud.com>
2022-05-22 13:15:09 +0200

    feat: sytled LayoutCar

--- libs/layout/card/src/lib/LayoutCard.tsx
+++ libs/layout/card/src/lib/LayoutCard.tsx
@@ -1,12 +1,16 @@
+import { ReactChild } from 'react';
 import styles from './LayoutCard.module.css';
 
 /* eslint-disable-next-line */
-export interface LayoutCardProps {}
+export interface LayoutCardProps {
+  children: ReactChild;
+}
 
 export function LayoutCard(props: LayoutCardProps) {
+  const { children } = props;
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to LayoutCard!</h1>
+    <div className="grid grid-flow-row-dense grid-cols-1 grid-rows-3 gap-4 pt-6 md:grid-cols-3 lg:grid-cols-4">
+      {children}
     </div>
   );
 }
