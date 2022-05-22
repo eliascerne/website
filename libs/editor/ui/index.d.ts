25955fdb1830d5e0191ee5acccdcc54edd25da8f
erias04 <github.eliascerne@icloud.com>
2022-05-22 15:29:40 +0200

    feat: added custom typings for editorjs plugins

---
+++ libs/editor/ui/index.d.ts
@@ -0,0 +1,74 @@
+declare module '@editorjs/header' {
+  import Header from '@editorjs/header';
+  export = Header;
+}
+
+declare module '@editorjs/list' {
+  import List from '@editorjs/list';
+  export = List;
+}
+
+declare module '@editorjs/table' {
+  import Table from '@editorjs/table';
+  export = Table;
+}
+
+declare module '@editorjs/embed' {
+  import Embed from '@editorjs/embed';
+  export = Embed;
+}
+
+declare module '@editorjs/warning' {
+  import Warning from '@editorjs/warning';
+  export = Warning;
+}
+
+declare module '@editorjs/code' {
+  import Code from '@editorjs/code';
+  export = Code;
+}
+
+declare module '@editorjs/link' {
+  import Links from '@editorjs/link';
+  export = Links;
+}
+
+declare module '@editorjs/image' {
+  import Image from '@editorjs/image';
+  export = Image;
+}
+
+declare module '@editorjs/raw' {
+  import Raw from '@editorjs/raw';
+  export = Raw;
+}
+
+declare module '@editorjs/quote' {
+  import Quote from '@editorjs/quote';
+  export = Quote;
+}
+
+declare module '@editorjs/marker' {
+  import Marker from '@editorjs/marker';
+  export = Marker;
+}
+
+declare module '@editorjs/checklist' {
+  import CheckList from '@editorjs/checklist';
+  export = CheckList;
+}
+
+declare module '@editorjs/delimiter' {
+  import Delimiter from '@editorjs/delimiter';
+  export = Delimiter;
+}
+
+declare module '@editorjs/inline-code' {
+  import InlineCode from '@editorjs/inline-code';
+  export = InlineCode;
+}
+
+declare module '@editorjs/simple-image' {
+  import SimpleImage from '@editorjs/simple-image';
+  export = SimpleImage;
+}
