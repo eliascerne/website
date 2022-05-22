a7811c63ba3482f2ab5dd10d5284a12a9e99ddcc
erias04 <github.eliascerne@icloud.com>
2022-05-22 20:38:51 +0200

    feat: enable saving text

--- libs/editor/ui/src/lib/EditorUi.tsx
+++ libs/editor/ui/src/lib/EditorUi.tsx
@@ -1,6 +1,7 @@
 import { createReactEditorJS } from 'react-editor-js';
 import { EditorConfig } from '@eliascerne/editor/ui';
 import dynamic from 'next/dynamic';
+import { useRef, useCallback } from 'react';
 
 import styles from './EditorUi.module.css';
 
@@ -9,16 +10,34 @@ const EditorJs = dynamic(() => import('react-editor-js') as any, {
 });
 
 /* eslint-disable-next-line */
-export interface EditorUiProps {}
+export interface EditorUiProps {
+  text: any;
+  setText: any;
+}
 
 const ReactEditorJS = createReactEditorJS();
 
 export function EditorUi(props: EditorUiProps) {
+  let { text, setText } = props;
+  const editorCore = useRef(null);
+  const handleInitialize = useCallback((instance: any) => {
+    editorCore.current = instance;
+  }, []);
+
+  const handleSave = useCallback(async () => {
+    let editorData: any;
+    editorData = editorCore?.current;
+    const editorSaveData = await editorData.save();
+    setText(editorSaveData.blocks);
+  }, []);
+
   return (
     <div className="text-white">
       <ReactEditorJS
         holder="editor"
         // tools={EditorConfig}
+        onInitialize={handleInitialize}
+        onChange={handleSave}
         defaultValue={{
           time: 1635603431943,
           blocks: [
