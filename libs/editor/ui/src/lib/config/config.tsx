d1a2040759861db6856cef8c764a2f98959277a2
erias04 <github.eliascerne@icloud.com>
2022-05-22 15:30:22 +0200

    feat: added editojs config file

--- libs/editor/ui/src/lib/config/config.tsx
+++ libs/editor/ui/src/lib/config/config.tsx
@@ -1,14 +1,86 @@
-import styles from './config.module.css';
+// import dynamic from 'next/dynamic';
+
+// const Embed = dynamic((() => import('@editorjs/embed')) as any, {
+//   ssr: false,
+// });
+// const Table = dynamic((() => import('@editorjs/table')) as any, {
+//   ssr: false,
+// });
+// const Warning = dynamic((() => import('@editorjs/warning')) as any, {
+//   ssr: false,
+// });
+// const List = dynamic((() => import('@editorjs/list')) as any, {
+//   ssr: false,
+// });
+// const Code = dynamic((() => import('@editorjs/code')) as any, {
+//   ssr: false,
+// });
+// const LinkTool = dynamic((() => import('@editorjs/link')) as any, {
+//   ssr: false,
+// });
+// const Image = dynamic((() => import('@editorjs/image')) as any, {
+//   ssr: false,
+// });
+// const Raw = dynamic((() => import('@editorjs/raw')) as any, {
+//   ssr: false,
+// });
+// const Header = dynamic((() => import('@editorjs/header')) as any, {
+//   ssr: false,
+// });
+// const Quote = dynamic((() => import('@editorjs/quote')) as any, {
+//   ssr: false,
+// });
+// const Marker = dynamic((() => import('@editorjs/marker')) as any, {
+//   ssr: false,
+// });
+// const CheckList = dynamic((() => import('@editorjs/checklist')) as any, {
+//   ssr: false,
+// });
+// const Delimiter = dynamic((() => import('@editorjs/delimiter')) as any, {
+//   ssr: false,
+// });
+// const InlineCode = dynamic((() => import('@editorjs/inline-code')) as any, {
+//   ssr: false,
+// });
+// const SimpleImage = dynamic((() => import('@editorjs/simple-image')) as any, {
+//   ssr: false,
+// });
 
 /* eslint-disable-next-line */
-export interface ConfigProps {}
+export interface EditorConfigProps {}
 
-export function Config(props: ConfigProps) {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to Config!</h1>
-    </div>
-  );
-}
+// import Embed from '@editorjs/embed';
+// import Table from '@editorjs/table';
+// import List from '@editorjs/list';
+// import Warning from '@editorjs/warning';
+// import Code from '@editorjs/code';
+// import LinkTool from '@editorjs/link';
+// import Image from '@editorjs/image';
+// import Raw from '@editorjs/raw';
+// import Header from '@editorjs/header';
+// import Quote from '@editorjs/quote';
+// import Marker from '@editorjs/marker';
+// import CheckList from '@editorjs/checklist';
+// import Delimiter from '@editorjs/delimiter';
+// import InlineCode from '@editorjs/inline-code';
+// import SimpleImage from '@editorjs/simple-image';
 
-export default Config;
+export const EditorConfig = {
+  // embed: Embed,
+  // table: Table,
+  // marker: Marker,
+  // list: List,
+  // warning: Warning,
+  // code: Code,
+  // linkTool: LinkTool,
+  // image: Image,
+  // raw: Raw,
+  // header: Header,
+  // quote: Quote,
+  // checklist: CheckList,
+  // delimiter: Delimiter,
+  // inlineCode: InlineCode,
+  // simpleImage: SimpleImage,
+};
+
+export default EditorConfig;
