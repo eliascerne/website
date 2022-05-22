c7095bd57d6f57304d25f2d23de4b499ba6d101a
erias04 <github.eliascerne@icloud.com>
2022-05-22 13:22:31 +0200

    feat: added cards to blog

--- apps/website/pages/blog/index.tsx/index.tsx
+++ apps/website/pages/blog/index.tsx/index.tsx
@@ -10,7 +10,62 @@ export function Blog(props: BlogProps) {
   return (
     <Fragment>
       <CardsUi
+        size="small"
+        type="blog"
+        image="https://cdn.pocket-lint.com/r/s/970x/assets/images/158839-laptops-review-apple-macbook-pro-14-inch-review-image2-ribxgzs9jt-jpg.webp"
+        title="Why serverless is the way to go"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        author="Elias Cerne"
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
         size="medium"
+        type="blog"
+        image="https://cdn.pocket-lint.com/r/s/970x/assets/images/158839-laptops-review-apple-macbook-pro-14-inch-review-image2-ribxgzs9jt-jpg.webp"
+        title="Why serverless is the way to go"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        author="Elias Cerne"
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
+        size="medium"
+        type="blog"
+        image="https://cdn.pocket-lint.com/r/s/970x/assets/images/158839-laptops-review-apple-macbook-pro-14-inch-review-image2-ribxgzs9jt-jpg.webp"
+        title="Why serverless is the way to go"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        author="Elias Cerne"
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
+        size="medium"
+        type="blog"
+        image="https://cdn.pocket-lint.com/r/s/970x/assets/images/158839-laptops-review-apple-macbook-pro-14-inch-review-image2-ribxgzs9jt-jpg.webp"
+        title="Why serverless is the way to go"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        author="Elias Cerne"
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
+        size="small"
+        type="blog"
+        image="https://cdn.pocket-lint.com/r/s/970x/assets/images/158839-laptops-review-apple-macbook-pro-14-inch-review-image2-ribxgzs9jt-jpg.webp"
+        title="Why serverless is the way to go"
+        description="Tailwindcss is being used for fast and ✨beautiful✨ styling like this website is."
+        author="Elias Cerne"
+        buttonText="Visit"
+        buttonIcon={true}
+        link="https://tailwindcss.com"
+      />
+      <CardsUi
+        size="small"
         type="blog"
         image="https://cdn.pocket-lint.com/r/s/970x/assets/images/158839-laptops-review-apple-macbook-pro-14-inch-review-image2-ribxgzs9jt-jpg.webp"
         title="Why serverless is the way to go"
